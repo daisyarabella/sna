35a034d1611c4d753d992c3f325a6f6fc3f949e6
Daisy Connor <daisy.connor@student.manchester.ac.uk>
2017-02-13 15:56:09 +0000

    started complex adoption

--- src/complexAdoption.java
+++ src/complexAdoption.java
@@ -1,4 +1,4 @@
-/*import java.io.FileWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
@@ -12,61 +12,127 @@ import org.graphstream.graph.Node;
 import org.graphstream.graph.Path;
 import org.graphstream.graph.implementations.*;
 
+import javax.swing.JTextArea;
+import javax.swing.JFrame;
 
 public class complexAdoption {
   static int Yt = 0; // Y(t)
   static int Ytadd1 = 0; // Y(t+1)
-  static int extAdoptionCount = 0;
   static int intAdoptionCount = 0;
+  static int extAdoptionCount = 0;
   static int t = 0; 
-  static int halfAdoptedTime = 0;
-  static boolean termination = false;
- 	
+	
   // go through complex adoption process
-  public static Graph adopt(Graph g, double p, int graphSize, FileWriter timestepfw, FileWriter linearfw) throws IOException {
+  public static Graph adopt(Graph g, double p, int graphSize, int sleepTime, 
+                            FileWriter timestepfw, FileWriter linearfw) throws IOException {
     g.addAttribute("ui.stylesheet", stylesheet);
-    do {
-      termination = false;
-      if (Yt == 0) {
-        g = initAdoption(g, p);
+    int[] nodeDegrees = new int[graphSize];
+  
+    for (Node n:g) {
+      nodenodeDegrees[n.getDegree()];
+    }
+    
+
+    return g;
+  }
+  
+  // adopt the first few nodes externally
+  public static Graph initAdoption(Graph g, double p, int sleepTime) {
+    for (Node n:g) {
+      if (Math.random() < p) {
+        n.setAttribute("adopted");
+        n.setAttribute("ui.class", "adopted");
+	  sleep(sleepTime);
+   	Yt++;
+   	Ytadd1++;
+   	extAdoptionCount++;
       }
-      g = complexAdoption(g,p);
-      if ((extAdoptionCount+intAdoptionCount) >= (graphSize/2)) {
-	halfAdoptedTime = t;
-        System.out.println("Timesteps to adopt >=50% graph: " + halfAdoptedTime);
+    }
+    return g;
+  }
+
+/*public class simpleAdoption {
+  static int Yt = 0; // Y(t)
+  static int Ytadd1 = 0; // Y(t+1)
+  static int intAdoptionCount = 0;
+  static int extAdoptionCount = 0;
+  static int t = 0; 
+  static boolean internalAdoptionHappen = false;
+	
+  // go through simple adoption process
+  public static Graph adopt(Graph g, double p, int graphSize, int sleepTime, 
+                            FileWriter timestepfw, FileWriter linearfw) throws IOException {
+    g.addAttribute("ui.stylesheet", stylesheet);
+    Yt = 0; // Y(t)
+    Ytadd1 = 0; // Y(t+1)
+    intAdoptionCount = 0;
+    extAdoptionCount = 0;
+    t = 0; 
+    internalAdoptionHappen = false;
+
+    int[] totalAdopters = new int[10*graphSize];
+    int[] extAdopters = new int[10*graphSize];
+    int[] intAdopters = new int[10*graphSize];
+
+    JTextArea textArea = GUI.makeTimestepDataGUI();    
+
+    do {
+      internalAdoptionHappen = false; 
+      if (Yt == 0) {
+        g = initAdoption(g, p, sleepTime);
+      }
+      g = internalAdoption(g, p, sleepTime);
+      if (!internalAdoptionHappen) {       			
+        g = externalAdoption(g, p, sleepTime);			
       }
 
       //exportCSV files
       timestepfw.write(t + "," + Yt + "," + extAdoptionCount + "," + intAdoptionCount + "\n");
       linearfw.write(Ytadd1-Yt + "," + 1 + "," + Yt + "," + Yt*Yt + "\n");
-      System.out.println("t: " +t+ "\t Y(t+1): " +Ytadd1+ "\t Y(t): " +Yt);
+      
+      // print timestep data to terminal and timestep data GUI
+      //System.out.println("t: " +t+ "\t Y(t+1): " +Ytadd1+ "\t Y(t): " +Yt);
+      textArea.append("t: " +t+ "\t Y(t+1): " +Ytadd1+ "\t Y(t): " +Yt+ "\t No. External Adoptions: " +extAdoptionCount+ "\t No. Internal Adoptions: " + intAdoptionCount +"\n");
+
+      // add the timestep data to int[] arrays for display in plot
+      totalAdopters[t] = Yt;
+      extAdopters[t] = extAdoptionCount;
+      intAdopters[t] = intAdoptionCount;
       t++;
-    } while (!termination);
-     	  
-    timestepfw.close();
-    linearfw.close();
-    System.out.println("External adoptions: " + extAdoptionCount + ", Internal adoptions: " +intAdoptionCount);
-    System.out.println("Finished");
-    return g;
+      } while (Yt < graphSize);
+      
+      timestepfw.close();
+      linearfw.close();
+
+      LineChart lineChart = new LineChart("Plot - adoption over time", "Number of adoptions over time", 
+                                          totalAdopters, extAdopters, intAdopters, t);
+      lineChart.setSize(600,350);
+      lineChart.show();
+
+      //System.out.println("External adoptions: " + extAdoptionCount + ", Internal adoptions: " +intAdoptionCount);
+      //System.out.println("Finished");
+      //textArea.append("External adoptions: " + extAdoptionCount + ", Internal adoptions: " +intAdoptionCount+"\n");
+      textArea.append("Finished\n");
+      return g;
   }
 
   // adopt the first few nodes externally
-  public static Graph initAdoption(Graph g, double p) {
+  public static Graph initAdoption(Graph g, double p, int sleepTime) {
     for (Node n:g) {
       if (Math.random() < p) {
         n.setAttribute("adopted");
-       	n.setAttribute("ui.class", "adopted");
-	sleep();
+        n.setAttribute("ui.class", "adopted");
+	sleep(sleepTime);
    	Yt++;
    	Ytadd1++;
    	extAdoptionCount++;
-   	}
+      }
     }
     return g;
   }
     
   // method to make all neighbours of adopted nodes adopted
-  private static Graph internalAdoption(Graph g, double p) {
+  private static Graph internalAdoption(Graph g, double p, int sleepTime) {
     Yt = Ytadd1;
     for (Node n:g) {
       // if node has adopted, getNeighbors of node
@@ -80,42 +146,44 @@ public class complexAdoption {
     	    neighbors.add(thisNeighbor);
     	  }
     	}
-
-	//neighbours.size();
-    	// assign p% neighbors with adopted attribute
+    	
+        // assign all neighbors with adopted attribute
     	for (Node neighbor : neighbors) {
-    	  if (Math.random() < p && !neighbor.hasAttribute("adopted")) {
+    	  if (!neighbor.hasAttribute("adopted")) {
     	    neighbor.setAttribute("adopted"); 
-            neighbor.setAttribute("ui.class", "adopted");
-            sleep();
+    	    neighbor.setAttribute("ui.class", "adopted");
+	        sleep(sleepTime);
     	    Ytadd1++;
-            intAdoptionCount++;
-            internalAdoptionHappen = true;
-    	  }
-    	} 
+    	    intAdoptionCount++;
+    	    internalAdoptionHappen = true;
+          }
+        } 
       }
     }
     return g;
   }
 
-  private static Graph externalAdoption(Graph g, double p) {
+  private static Graph externalAdoption(Graph g, double p, int sleepTime) {
     Yt = Ytadd1;
     for (Node n:g) {
       if (!n.hasAttribute("adopted")) {
-    	if (Math.random() < p) {
+        if (Math.random() < p) {
     	  n.setAttribute("adopted");
     	  n.setAttribute("ui.class", "adopted");
-	  sleep();
+	      sleep(sleepTime);
     	  Ytadd1++;
     	  extAdoptionCount++;
     	}
       }
     }
     return g;
-  } 
+  }*/ 
 
-  protected static void sleep() {
-    try { Thread.sleep(150); } catch (Exception e) {}
+  protected static void sleep(int sleepTime) {
+    try {
+      Thread.sleep(sleepTime); 
+      //Thread.currentThread().interrupt(); 
+    } catch (Exception e) {}
   }
 
   protected static String stylesheet =
@@ -124,5 +192,5 @@ public class complexAdoption {
     "}" +
     "node.adopted {" +
     "	fill-color: orange;" +
-    "}";       
-}*/
+    "}";      
+}
